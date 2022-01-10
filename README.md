ac455bfffa363aa6fc1dca203343d0a965956f0b
Dom Rodriguez <shymega@shymega.org.uk>
2022-01-10 18:59:14 +0000

    Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ README.md
@@ -0,0 +1,16 @@
+# morph-fw-rs
+
+This is an experimental Rust-based embedded firmware for the [Morph
+InfiniConnect][morph_earbuds] earbuds's charging case.
+
+It is designed to be battery-efficient, and will be using interrupts rather than
+polling.
+
+This is all a work in progress right now. Here be dragons!
+
+## License
+
+This is [licensed under][license] the MIT license.
+
+[morph_earbuds]: https://www.kickstarter.com/projects/morphaudio/morph-rapidly-switch-audio-across-infinite-devices-any-os
+[license]: /LICENSE
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+//! Firmware for `morph-fw-rs`.
+#![deny(
+    warnings,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    clippy::all,
+    clippy::cargo,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_import_braces,
+    unused_qualifications,
+    unused_extern_crates,
+    variant_size_differences
+)]
+
+use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+
+fn main() {
+    // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
+    // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
+    esp_idf_sys::link_patches();
+}
